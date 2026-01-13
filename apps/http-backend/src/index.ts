f5758c9281cde9267cbfabf17fdc84290e7d2e69
Baibhav Raj <r.baibhav2023@gmail.com>
2026-01-13 11:23:58 +0000

    initializing

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());  
+
+
+
+app.listen(PORT, () => {
+  console.log(`listeing to port ${PORT}`)
+})
